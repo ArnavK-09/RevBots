d12b27ee15711f6dbd0e0903a37da359bc1e4de7
Arnav K <arnavkaushal09@gmail.com>
2023-06-16 19:25:09 +0000

    Revert "fix bot invite link"
    This reverts commit 7baf5bbd99f857e6a1120b391d9b0e65d23888f3.

--- website/src/routes/bot/+server.ts
+++ website/src/routes/bot/+server.ts
@@ -4,5 +4,5 @@ import type { RequestHandler } from './$types';
 
 // GET /discord
 export const GET = (() => {
-	throw redirect(301, 'https://app.revolt.chat/bot/01H08716FW8WXP2RGH98S5VSXJ');
+	throw redirect(301, '/@RevBots/invite');
 }) satisfies RequestHandler;
